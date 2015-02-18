40fe0c8779753ba5b16c1c19831f4b99676b3f29
mark lybarger <mlybarger@gmail.com>
2015-02-18 11:58:14 -0500

    changes to programattically configure the logger.

--- src/main/java/org/test/example/App.java
+++ src/main/java/org/test/example/App.java
@@ -1,8 +1,13 @@
 package org.test.example;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
+import org.apache.logging.log4j.core.Filter;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.filter.DynamicThresholdFilter;
 
 
 /**
@@ -15,6 +20,19 @@ public class App
 	
     public static void main( String[] args )
     {
+    	// Logging only to debug for household User1.
+    	
+    	// programmatically configure.
+    	
+    	final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+    	Configuration cfg = ctx.getConfiguration();
+    	Filter filter = cfg.getFilter();
+    	DynamicThresholdFilter dtf = (DynamicThresholdFilter) filter;
+    	dtf.getLevelMap().put("User12", Level.DEBUG);
+    	
+    	//System.setProperty("DebugHousehold", "User1");
+    	//ThreadContext.put("DebugHousehold", "User1");
+    	
         ThreadContext.put("household", "User12");
         LOG.error("error", new java.lang.Exception());
         LOG.debug("one");
