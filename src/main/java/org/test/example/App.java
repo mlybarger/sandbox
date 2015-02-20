44f4363d2043383a0345c3bd387940530a2de234
mark lybarger <mlybarger@gmail.com>
2015-02-20 10:51:17 -0500

    add configuration.

--- src/main/java/org/test/example/App.java
+++ src/main/java/org/test/example/App.java
@@ -18,33 +18,49 @@ public class App
 {
 	private static final Logger LOG = LogManager.getLogger(App.class);
 	
-    public static void main( String[] args )
-    {
-    	// Logging only to debug for household User1.
-    	
-    	// programmatically configure.
-    	
+	private static String users = System.getProperty("household");
+	
+	private static synchronized void configureLogger() {
+		// synchronized method to ensure only one update the logger occurs at a time.
+		String [] userArray = users.split(",");
     	final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
     	Configuration cfg = ctx.getConfiguration();
     	Filter filter = cfg.getFilter();
     	DynamicThresholdFilter dtf = (DynamicThresholdFilter) filter;
-    	dtf.getLevelMap().put("User12", Level.DEBUG);
+    	for (String user: userArray){
+    		if (dtf.getLevelMap().get(user) == null) {
+    			dtf.getLevelMap().put(user, Level.DEBUG);
+    		}
+    	}
+    	// TODO: remove items in levelMap that aren't in users list.
+	}
+	
+    public static void main( String[] args )
+    {
+    	// Logging only to debug for household User1.
+
+    	System.setProperty("household", "40001,40006");
     	
-    	//System.setProperty("DebugHousehold", "User1");
-    	//ThreadContext.put("DebugHousehold", "User1");
-    	
-        ThreadContext.put("household", "User12");
+    	// if the system property has changed, programmatically configure logger.
+    	// this could be first in servlet method or some such.
+    	if (users != System.getProperty("household")) {
+    		users = System.getProperty("household");
+        	configureLogger();
+    	}
+    	    	
+    	// the context.put will only be in the servlet/mdb once.
+        ThreadContext.put("household", "40001");
         LOG.error("error", new java.lang.Exception());
         LOG.debug("one");
-        ThreadContext.put("household", "User1");
+        ThreadContext.put("household", "40001");
         LOG.debug("two");
         LOG.info("INFO");
-        ThreadContext.put("household", "User12");
-        LOG.error("error", new java.lang.Exception());
-        ThreadContext.put("household", "User12");
+        ThreadContext.put("household", "40005");
+        LOG.error("5 error", new java.lang.Exception());
+        ThreadContext.put("household", "40006");
         LOG.debug("three");
         LOG.info("INFO 2");
-        ThreadContext.put("household", "User1");
+        ThreadContext.put("household", "40001");
         LOG.debug("four");
         
     }
